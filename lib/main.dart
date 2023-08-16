a2668c04a3827869819a08caae1c011baed70543
Nazim <nazim@jrfintel.com>
2023-08-16 16:56:00 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'screens/Dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'My Flutter App',
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: Dashboard()
+    );
+  }
+}
+
+
---
+++ lib/screens/Dashboard.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard'.toUpperCase()),
+        backgroundColor: Colors.blueGrey,),
+      body:  Center(
+        child: Text.rich(
+          TextSpan(text: 'My',
+            children: [
+              TextSpan(text: 'Flutter', style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+              TextSpan(text: 'App', style: TextStyle(fontSize: 30.0, color: Colors.blue)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
